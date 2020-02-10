fe9c304f98ed1a349c40a0faf40f334fb01f1457
dusk <exinyijiu@163.com>
2020-02-10 20:22:35 +0800

    token done

---
+++ src/view/usercenter/user/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+  <div class="head-sea">
+      <Row>
+        <Col span="12"><Input search enter-button placeholder="搜索" style="width: 300px" /></Col>
+        <Col span="12" style="text-align:right"><Button type="success">新增用户</Button></Col>
+     </Row>
+  </div>
+     <Table border :columns="columnsUser" :data="userList"></Table>
+  </div>
+</template>
+
+<script>
+import { getUserInfo } from '@/api/user'
+export default {
+    name: 'user',
+    data() {
+        return {
+                 columnsUser: [
+                    {
+                        title: '用户名',
+                        key: 'name'
+                    },
+                    {
+                        title: '邮箱',
+                        key: 'email'
+                    },
+                    {
+                        title: '角色',
+                        key: 'role'
+                    },
+                    {
+                        title: '属组',
+                        key: 'group'
+                    }
+                ],
+                userList: []
+        }
+    },
+    methods: {
+        handleGetUserList() {
+            getUserInfo().then(resp => {
+                console.log(resp)
+                this.userList = resp.data
+            })
+        }
+    }
+}
+</script>
+
+<style>
+.head-sea {
+    margin-bottom: 10px;
+}
+
+</style>
